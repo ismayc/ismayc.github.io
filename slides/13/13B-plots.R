e7cd37cc12744263f1826588c6c385e2c1316049
ismayc <chester.ismay@gmail.com>
2016-04-28 17:49:20 -0700

    Added differences histogram

--- slides/13/13B-plots.R
+++ slides/13/13B-plots.R
@@ -4,9 +4,16 @@ homedir <- "/Users/Chester/Google Drive/ismayc.github.io/teaching/diagrams/"
 
 # 1
 response <- rnorm(1000)
-dataframe <- data.frame(dataframe = response)
 qplot(x = response, geom = "histogram", binwidth = 0.5, col = I("white")) + theme(text = element_text(size = 30))
 ggsave(paste0(homedir, "01-histogram.png"), dpi = 1000)
+
+# 2
+response <- c(rnorm(1000, mean = 10, sd = 2), rnorm(1000, mean = 5, sd = 1))
+explanatory <- c(rep("group1", 1000), rep("group2", 1000))
+dataframe <- data.frame(explanatory, response) 
+qplot(x = explanatory, y = response, data = dataframe, geom = "boxplot") + theme(text = element_text(size = 30))
+ggsave(paste0(homedir, "02-side_by_side_boxplot.png"), dpi = 1000)
+
 
 # 7
 response <- c(rep("group1", 57), rep("group2", 75), rep("group3", 40), rep("group4", 50))
