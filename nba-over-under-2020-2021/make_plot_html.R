51aa41bab44f6bd2b36e1905214c42951baa3669
Chester Ismay <chester.ismay@datarobot.com>
2021-02-06 19:09:45 -0800

    Updated 2021-02-06 19:09:45

--- nba-over-under-2020-2021/make_plot_html.R
+++ nba-over-under-2020-2021/make_plot_html.R
@@ -10,7 +10,7 @@ rmarkdown::render(
 )
 cat(glue::glue("Completed at {Sys.time()}\n\n"))
 
-Sys.sleep(time = 10)
+#Sys.sleep(time = 10)
 
 system("git add --all")
 system(paste0('git commit -m "Updated ', Sys.time(), '"'))
