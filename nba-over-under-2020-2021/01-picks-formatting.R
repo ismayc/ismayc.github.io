3c8a661d94cdae0168ec29f1ae3c1fb2770bb369
Chester Ismay <chester.ismay@datarobot.com>
2021-04-02 12:25:47 -0700

    Updated 2021-04-02 12:25:47

--- nba-over-under-2020-2021/01-picks-formatting.R
+++ nba-over-under-2020-2021/01-picks-formatting.R
@@ -6,6 +6,20 @@ projections <- read_excel(path = "picks.xlsx", sheet = "projections") %>%
   mutate(percentage_projection = win_projection / 72 * 100)
 meta <- read_excel(path = "picks.xlsx", sheet = "meta") 
 
+picks_wide <- picks %>% 
+#  unite("team_player", team:player, sep = "_") %>% 
+  unite("wage_choice", wage:choice, sep = " ") %>% 
+  pivot_wider(names_from = player, values_from = wage_choice) %>% 
+  inner_join(projections, by = "team") %>% 
+  inner_join(standings, by = c("team" = "team_name")) %>% 
+  mutate(percentage_projection = round(percentage_projection / 100, 3)) %>% 
+  select(-conference, -division, -`Games Back`, -differential) %>% 
+  mutate(loss_projection = 72 - win_projection, .after = win_projection) %>% 
+  mutate(outcome_determined = case_when(
+    wins > win_projection ~ "OVER",
+    losses > loss_projection ~ "UNDER",
+    TRUE ~ NA_character_))
+
 # Check picks follow rules
 #picks %>% arrange(player, desc(wage), choice) %>% View()
 
