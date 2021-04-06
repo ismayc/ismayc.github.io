eed1c9d05cb482d9afb8b0b62b21d59d2adfdd5b
Chester Ismay <chester.ismay@datarobot.com>
2021-04-06 07:53:08 -0700

    Tweaks

--- nba-over-under-2020-2021/explore-scenarios.R
+++ nba-over-under-2020-2021/explore-scenarios.R
@@ -3,19 +3,19 @@ library(googlesheets)
 
 expected <- read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vRVvszIE_nImQEeOG8684tsMhc72OkNb7QN9FDVSsagHpG3PnPQ_e4aQkyNdwt8pF27p6EgEztDvkVr/pub?gid=0&single=true&output=csv")
 
-testing <- expected %>% 
-  transmute(team, prob,
-            Chester = if_else(chester_choice == expected, Chester, -Chester),
-            Jake = if_else(jake_choice == expected, Jake, -Jake),
-            Jenelle = if_else(jenelle_choice == expected, Jenelle, -Jenelle),
-            Adonis	= if_else(adonis_choice == expected, Adonis, -Adonis),
-            Mike	= if_else(mike_choice == expected, Mike, -Mike),
-            Mary	= if_else(mary_choice == expected, Mary, -Mary),
-            Ryan = if_else(ryan_choice == expected, Ryan, -Ryan),
-            Phil	= if_else(phil_choice == expected, Phil, -Phil)) %>% 
-  pivot_longer(cols = Chester:Phil,
-               names_to = "player",
-               values_to = "expected_points")
+# testing <- expected %>% 
+#   transmute(team, prob,
+#             Chester = if_else(chester_choice == expected, Chester, -Chester),
+#             Jake = if_else(jake_choice == expected, Jake, -Jake),
+#             Jenelle = if_else(jenelle_choice == expected, Jenelle, -Jenelle),
+#             Adonis	= if_else(adonis_choice == expected, Adonis, -Adonis),
+#             Mike	= if_else(mike_choice == expected, Mike, -Mike),
+#             Mary	= if_else(mary_choice == expected, Mary, -Mary),
+#             Ryan = if_else(ryan_choice == expected, Ryan, -Ryan),
+#             Phil	= if_else(phil_choice == expected, Phil, -Phil)) %>% 
+#   pivot_longer(cols = Chester:Phil,
+#                names_to = "player",
+#                values_to = "expected_points")
 
 # Simulate
 lookup_table <- expected %>% 
@@ -24,10 +24,10 @@ lookup_table <- expected %>%
 picks <- read_excel(path = "picks.xlsx", sheet = "picks")
 
 set.seed(NULL)
+start_time <- Sys.time()
 outcome_sims <- map_dfr(
   1:10000, 
   ~ {
-    #rerun(.n = 1000, {
     sim_prep <- picks %>% 
       inner_join(lookup_table, by = "team") %>% 
       select(team, expected, prob) %>% 
@@ -56,12 +56,15 @@ outcome_sims <- map_dfr(
     out
   }
 ) 
+end_time <- Sys.time()
+end_time - start_time
 
 outcome_sims %>% 
   group_by(player) %>% 
   summarize(median_expected_total = median(expected_total),
             mean_expected_total = mean(expected_total),
             sd_expected_total = sd(expected_total),
+            median_rank = median(rank),
             mean_rank = mean(rank)) %>% 
   arrange(desc(median_expected_total))
 
