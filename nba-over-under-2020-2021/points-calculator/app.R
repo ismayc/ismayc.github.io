a1a539af54fc9639a681137dab9412666fa0fd74
Chester Ismay <chester.ismay@gmail.com>
2021-05-05 11:22:20 -0700

    Updated 2021-05-05 11:22:20

--- nba-over-under-2020-2021/points-calculator/app.R
+++ nba-over-under-2020-2021/points-calculator/app.R
@@ -1,11 +1,4 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+# https://chesterismay.shinyapps.io/over-under-points-calculator/
 
 library(shiny)
 library(readxl)
