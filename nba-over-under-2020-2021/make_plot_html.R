5103396d3023a128fcd94f26b89ec9fe94405d40
Chester Ismay <chester.ismay@gmail.com>
2021-05-05 10:50:46 -0700

    Updated 2021-05-05 10:50:46

--- nba-over-under-2020-2021/make_plot_html.R
+++ nba-over-under-2020-2021/make_plot_html.R
@@ -16,6 +16,7 @@ date_modified <- file.info(file.path("..", "2021-nba-over-under.html")) %>%
 
 system("git config --global user.name 'Chester Ismay'")
 system("git remote set-url origin git@github.com:ismayc/ismayc.github.io.git")
+system("git config pull.rebase false")
 system("git pull")
 
 if (date_modified != Sys.Date() || update_page) {
