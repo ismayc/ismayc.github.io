005b235314982db6f9dc09c7adb271330b093c0b
Chester Ismay <chester.ismay@gmail.com>
2022-03-27 04:15:45 -0700

    Updated 2022-03-27 04:15:44

--- nba-over-under-2021-2022/rosters/career_totals.R
+++ nba-over-under-2021-2022/rosters/career_totals.R
@@ -16,12 +16,96 @@ cat("\n")
 
 tic()
 career_stats <- nbastatR::players_careers(
-  player_ids = nba_players$idPlayer,
+  player_ids = nba_players$idPlayer[501:1000],
   modes = "Totals"
 )
 toc()
 
 readr::write_rds(
   dataPlayerCareerTotalsRegularSeason,
-  paste0("~/Desktop/regular_season_career-", Sys.Date(), ".rds")
+  paste0("~/Desktop/regular_season_career-501to1000-", Sys.Date(), ".rds")
+)
+
+tic()
+career_stats <- nbastatR::players_careers(
+  player_ids = nba_players$idPlayer[1001:1500],
+  modes = "Totals"
+)
+toc()
+
+readr::write_rds(
+  dataPlayerCareerTotalsRegularSeason,
+  paste0("~/Desktop/regular_season_career-1001to1500-", Sys.Date(), ".rds")
+)
+
+tic()
+career_stats <- nbastatR::players_careers(
+  player_ids = nba_players$idPlayer[1501:2000],
+  modes = "Totals"
+)
+toc()
+
+readr::write_rds(
+  dataPlayerCareerTotalsRegularSeason,
+  paste0("~/Desktop/regular_season_career-1501to2000-", Sys.Date(), ".rds")
+)
+
+tic()
+career_stats <- nbastatR::players_careers(
+  player_ids = nba_players$idPlayer[2001:2500],
+  modes = "Totals"
+)
+toc()
+
+readr::write_rds(
+  dataPlayerCareerTotalsRegularSeason,
+  paste0("~/Desktop/regular_season_career-2001to2500-", Sys.Date(), ".rds")
+)
+
+tic()
+career_stats <- nbastatR::players_careers(
+  player_ids = nba_players$idPlayer[2501:3000],
+  modes = "Totals"
+)
+toc()
+
+readr::write_rds(
+  dataPlayerCareerTotalsRegularSeason,
+  paste0("~/Desktop/regular_season_career-2501to3000-", Sys.Date(), ".rds")
+)
+
+tic()
+career_stats <- nbastatR::players_careers(
+  player_ids = nba_players$idPlayer[3001:3500],
+  modes = "Totals"
+)
+toc()
+
+readr::write_rds(
+  dataPlayerCareerTotalsRegularSeason,
+  paste0("~/Desktop/regular_season_career-3001to3500-", Sys.Date(), ".rds")
+)
+
+tic()
+career_stats <- nbastatR::players_careers(
+  player_ids = nba_players$idPlayer[3501:4000],
+  modes = "Totals"
+)
+toc()
+
+readr::write_rds(
+  dataPlayerCareerTotalsRegularSeason,
+  paste0("~/Desktop/regular_season_career-3501to4000-", Sys.Date(), ".rds")
+)
+
+tic()
+career_stats <- nbastatR::players_careers(
+  player_ids = nba_players$idPlayer[4001:4730],
+  modes = "Totals"
+)
+toc()
+
+readr::write_rds(
+  dataPlayerCareerTotalsRegularSeason,
+  paste0("~/Desktop/regular_season_career-4001to4730-", Sys.Date(), ".rds")
 )
