96a25681e695d11f2991fddff5a25e8690aeae9a
Chester Ismay <chester.ismay@gmail.com>
2023-11-05 13:18:28 -0800

    Again

--- nba-over-under-2023-2024/make_plot_html.R
+++ nba-over-under-2023-2024/make_plot_html.R
@@ -25,17 +25,18 @@ date_modified <- file.info(file.path("..", paste0(year, "-nba-over-under.html"))
   pull(ctime) %>% 
   as.Date()
 
-system("git config --global user.name 'Chester Ismay'")
-system("git remote set-url origin git@github.com:ismayc/ismayc.github.io.git")
-system("git config pull.rebase false")
-system("git pull")
+# system("git config --global user.name 'Chester Ismay'")
+# system("git remote set-url origin git@github.com:ismayc/ismayc.github.io.git")
+# system("git config pull.rebase false")
+# system("git pull")
 
 if (date_modified != Sys.Date() || update_page) {
   rmarkdown::render(
-    input = "make_plots.Rmd",  
+    input = "nba-over-under-2023-2024/make_plots.Rmd",  
     output_format = "html_document",
     output_file = paste0(year, "-nba-over-under.html"),
-    output_dir = "..",
+#    output_dir = "..",
+    output_dir = ".",
     quiet = TRUE
   )
 } else {
@@ -43,6 +44,6 @@ if (date_modified != Sys.Date() || update_page) {
 }
 cat(glue("Completed at {Sys.time() - lubridate::hours(8)}"), "Pacific time", "\n")
 
-system("git add --all")
-system(paste0('git commit -m "Updated ', Sys.time(), '"'))
-system("git push origin master")
+# system("git add --all")
+# system(paste0('git commit -m "Updated ', Sys.time(), '"'))
+# system("git push origin master")
