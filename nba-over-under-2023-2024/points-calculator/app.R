cbf3e9565b20da34e92b818ffaf70c9fca40cd15
Chester Ismay <chester.ismay@gmail.com>
2024-03-13 20:57:34 -1000

    Go back to fixed

--- nba-over-under-2023-2024/points-calculator/app.R
+++ nba-over-under-2023-2024/points-calculator/app.R
@@ -15,9 +15,7 @@ if (file.exists(paste0("determined_outcomes_",
                           as.Date(Sys.time()), 
                           ".rds"))
 } else {
-  temp <- read_rds(paste0("determined_outcomes_", 
-                          as.Date(Sys.time()) - lubridate::days(1), 
-                          ".rds"))
+  temp <- read_rds(paste0("determined_outcomes_2024-03-13.rds"))
 }
 
 determined <- temp |> 
