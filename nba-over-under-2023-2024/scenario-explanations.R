5cf7c918a5485f8e36a4ffc3452a606f56be9dea
Chester Ismay <chester.ismay@gmail.com>
2024-04-12 08:24:29 -0400

    Down to 2

--- nba-over-under-2023-2024/scenario-explanations.R
+++ nba-over-under-2023-2024/scenario-explanations.R
@@ -2,7 +2,7 @@ library(DBI)
 library(dbplyr)
 library(tidyverse)
 
-teams_remaining <- 4
+teams_remaining <- 2
 # Create a new SQLite database or open a connection to an existing one
 con <- dbConnect(RSQLite::SQLite(), dbname = "nba_scenarios.sqlite")
 
