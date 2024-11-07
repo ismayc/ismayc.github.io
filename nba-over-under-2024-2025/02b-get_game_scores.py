d8af654555a4cc13c3aecac95c0a77b78363cba7
Chet Ismay <chester.ismay@gmail.com>
2024-11-07 16:33:33 -0700

    Update schedule

--- nba-over-under-2024-2025/02b-get_game_scores.py
+++ nba-over-under-2024-2025/02b-get_game_scores.py
@@ -1,10 +1,11 @@
 # 2024-25 games
 
-from nba_api.stats.endpoints import leaguegamefinder
 year = '2024'
+
+from nba_api.stats.endpoints import leaguegamefinder
 gamefinder = leaguegamefinder.LeagueGameFinder()
 games = gamefinder.get_data_frames()[0]
 
 # Subset the games to when the last 4 digits of SEASON_ID were current year start.
-games_22 = games[games.SEASON_ID.str[-4:] == year]
-games_22.to_csv("current_year.csv")
+season_games = games[games.SEASON_ID.str[-4:] == year]
+season_games.to_csv("current_year.csv")
