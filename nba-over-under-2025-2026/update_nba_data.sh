677ce001c2fc96a5e2a6bcf97d64ce13acf815b7
Chester Ismay <chester.ismay@gmail.com>
2025-12-22 15:03:01 -0700

    Add nba data dl shell script

---
+++ nba-over-under-2025-2026/update_nba_data.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cd ~/Desktop/repos/ismayc.github.io/nba-over-under-2025-2026/nba-over-under-2025-2026
+
+python3 << 'EOF'
+year = '2025'
+from nba_api.stats.endpoints import leaguegamefinder
+gamefinder = leaguegamefinder.LeagueGameFinder()
+games = gamefinder.get_data_frames()[0]
+season_games = games[games.SEASON_ID.str[-4:] == year]
+season_games.to_csv("current_year.csv", index=False)
+print(f"Updated current_year.csv with {len(season_games)} games")
+EOF
+
+git add current_year.csv
+git commit -m "Update NBA data $(date +%Y-%m-%d)"
+git push origin master
