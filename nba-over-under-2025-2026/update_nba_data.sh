e75fe493e4a832fb3c517fc36e9fadff72a84c12
Chester Ismay <chester.ismay@gmail.com>
2025-12-23 13:32:48 -0700

    Add git pull

--- nba-over-under-2025-2026/update_nba_data.sh
+++ nba-over-under-2025-2026/update_nba_data.sh
@@ -11,6 +11,9 @@ season_games.to_csv("current_year.csv", index=False)
 print(f"Updated current_year.csv with {len(season_games)} games")
 EOF
 
+git config --local user.email "actions@github.com"
+git config --local user.name "GitHub Actions"
+git pull --rebase origin master
 git add current_year.csv
 git commit -m "Update NBA data $(date +%Y-%m-%d)"
 git push origin master
