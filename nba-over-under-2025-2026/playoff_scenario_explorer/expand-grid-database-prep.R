2734c072c00483aa4a66a7de3bf3d9ae4a74e40c
Chester Ismay <chester.ismay@gmail.com>
2026-02-11 11:12:10 -0700

    Update points calculator

--- nba-over-under-2025-2026/playoff_scenario_explorer/expand-grid-database-prep.R
+++ nba-over-under-2025-2026/playoff_scenario_explorer/expand-grid-database-prep.R
@@ -1,19 +1,18 @@
 library(tidyverse)
-
-# Setup disk.frame and allow it to use multiple cores
-library(disk.frame)
-setup_disk.frame(workers = 8)
-options(future.globals.maxSize = Inf)
+library(arrow)
 
 picks <- readxl::read_excel("picks.xlsx", sheet = "picks")
+
 picks_wide_points <- picks %>%
   select(team:wage) %>%
   pivot_wider(names_from = player,
               values_from = wage)
+
 picks_wide_choice <- picks %>%
   select(team, player, choice) %>%
   pivot_wider(names_from = player,
               values_from = choice)
+
 points_names <- names(picks_wide_points)[names(picks_wide_points) != "team"]
 choice_names <- names(picks_wide_choice)[names(picks_wide_choice) != "team"]
 
@@ -26,8 +25,10 @@ names(picks_wide_choice) <- c("team", choice_names)
 picks_wide_new <- picks_wide_points %>%
   inner_join(picks_wide_choice)
 
-picks_wide_new <- picks_wide_new[ , order(names(picks_wide_new))] %>%
+picks_wide_new <- picks_wide_new[, order(names(picks_wide_new))] %>%
   relocate(team, everything())
+
+write_parquet(picks_wide_new, "picks_wide_new.parquet")
 
 readr::write_rds(
   picks_wide_new, "picks_wide_new.rds")
